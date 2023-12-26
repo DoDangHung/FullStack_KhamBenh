e420fcdee3fc6f1bec9abea4dbf1b9a2a81b6964
Hung Do <dohungid00@gmail.com>
2023-12-26 22:00:12 +0100

    first commit

---
+++ KhamBenh_FrontEnd/src/containers/System/UserManage.js
@@ -0,0 +1,27 @@
+/** @format */
+
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return <div className="text-center">Manage users</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
---
+++ KhamBenh_FrontEnd/src/services/index.js
@@ -0,0 +1,3 @@
+/** @format */
+
+export { default as adminService } from "./adminService";
